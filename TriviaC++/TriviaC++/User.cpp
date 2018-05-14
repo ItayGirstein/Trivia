4078ed587882ec62b515a09e17b89f942e9506f2
tal22131 <tal22131@bitbucket.org>
2018-05-14 17:48:48 +0300

    user added

--- TriviaC++/TriviaC++/User.cpp
+++ TriviaC++/TriviaC++/User.cpp
@@ -7,6 +7,56 @@ User::User()
 }
 
 
+User::User(string, SOCKET)
+{
+}
+
 User::~User()
 {
+}
+
+void User::send(string)
+{
+
+}
+
+string User::gestUsername()
+{
+	return _username;
+}
+
+SOCKET User::getSocket()
+{
+	return _sock;
+}
+
+Room * User::getRoom()
+{
+	return _currRoom;
+}
+
+Game * User::getGame()
+{
+	return _currGame;
+}
+
+void User::setGame(Game* currGame)
+{
+	_currGame = currGame;
+}
+
+bool User::createRoom(int, string, int, int, int)
+{
+	_currRoom = new Room();
+	return false;
+}
+
+bool User::joinRoom(Room *)
+{
+	return false; // todo
+}
+
+void User::leaveRoom()
+{
+	_currGame = nullptr; // todo
 }
--- TriviaC++/TriviaC++/User.h
+++ TriviaC++/TriviaC++/User.h
@@ -6,9 +6,22 @@
 class User
 {
 public:
-	User();
+	User(string, SOCKET);
 	~User();
 
+
+	void send(string);
+	string gestUsername();
+	SOCKET getSocket();
+	Room* getRoom();
+	Game* getGame();
+	void setGame(Game* currGame);
+	void clearRoom();
+	bool createRoom(int, string, int, int, int);
+	bool joinRoom(Room*);
+	void leaveRoom();
+	int closeRoom();
+	bool leaveGame();
 
 
 private:
